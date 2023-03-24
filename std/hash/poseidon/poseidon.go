9f448cff679635a1ccb1dd363603688420523b58
Lucas Menéndez <hi@lucasmenendez.me>
2023-03-24 10:02:57 +0100

    make some poseidon helpers private and delete unused code

--- std/hash/poseidon/poseidon.go
+++ std/hash/poseidon/poseidon.go
@@ -6,13 +6,13 @@ import (
 	"github.com/consensys/gnark/frontend"
 )
 
-func Sigma(api frontend.API, in frontend.Variable) frontend.Variable {
+func sigma(api frontend.API, in frontend.Variable) frontend.Variable {
 	in2 := api.Mul(in, in)
 	in4 := api.Mul(in2, in2)
 	return api.Mul(in4, in)
 }
 
-func Ark(api frontend.API, in []frontend.Variable, c []*big.Int, r int) []frontend.Variable {
+func ark(api frontend.API, in []frontend.Variable, c []*big.Int, r int) []frontend.Variable {
 	out := make([]frontend.Variable, len(in))
 	for i, v := range in {
 		out[i] = api.Add(v, c[i+r])
@@ -20,7 +20,7 @@ func Ark(api frontend.API, in []frontend.Variable, c []*big.Int, r int) []fronte
 	return out
 }
 
-func Mix(api frontend.API, in []frontend.Variable, m [][]*big.Int) []frontend.Variable {
+func mix(api frontend.API, in []frontend.Variable, m [][]*big.Int) []frontend.Variable {
 	t := len(in)
 	out := make([]frontend.Variable, t)
 	for i := 0; i < t; i++ {
@@ -33,25 +33,11 @@ func Mix(api frontend.API, in []frontend.Variable, m [][]*big.Int) []frontend.Va
 	return out
 }
 
-func MixLast(api frontend.API, in []frontend.Variable, m [][]*big.Int, s int) frontend.Variable {
+func mixLast(api frontend.API, in []frontend.Variable, m [][]*big.Int, s int) frontend.Variable {
 	t := len(in)
 	out := frontend.Variable(0)
 	for j := 0; j < t; j++ {
 		out = api.Add(out, api.Mul(m[j][s], in[j]))
-	}
-	return out
-}
-
-func MixS(api frontend.API, in []frontend.Variable, s []*big.Int, r int) []frontend.Variable {
-	t := len(in)
-	out := make([]frontend.Variable, t)
-	lc := frontend.Variable(0)
-	for i := 0; i < t; i++ {
-		lc = api.Add(lc, api.Mul(s[(t*2-1)*r+i], in[i]))
-	}
-	out[0] = lc
-	for i := 1; i < t; i++ {
-		out[i] = api.Add(in[i], api.Mul(in[0], s[(t*2-1)*r+t+i-1]))
 	}
 	return out
 }
@@ -80,25 +66,25 @@ func Poseidon(api frontend.API, inputs []frontend.Variable, initialState fronten
 			state[j] = inputs[j-1]
 		}
 	}
-	state = Ark(api, state, c, 0)
+	state = ark(api, state, c, 0)
 
 	for r := 0; r < nRoundsF/2-1; r++ {
 		for j := 0; j < t; j++ {
-			state[j] = Sigma(api, state[j])
+			state[j] = sigma(api, state[j])
 		}
-		state = Ark(api, state, c, (r+1)*t)
-		state = Mix(api, state, m)
+		state = ark(api, state, c, (r+1)*t)
+		state = mix(api, state, m)
 	}
 
 	for j := 0; j < t; j++ {
-		state[j] = Sigma(api, state[j])
+		state[j] = sigma(api, state[j])
 	}
-	state = Ark(api, state, c, nRoundsF/2*t)
-	state = Mix(api, state, p)
+	state = ark(api, state, c, nRoundsF/2*t)
+	state = mix(api, state, p)
 
 	for r := 0; r < nRoundsP; r++ {
 
-		state[0] = Sigma(api, state[0])
+		state[0] = sigma(api, state[0])
 
 		state[0] = api.Add(state[0], c[(nRoundsF/2+1)*t+r])
 		newState0 := frontend.Variable(0)
@@ -115,18 +101,18 @@ func Poseidon(api frontend.API, inputs []frontend.Variable, initialState fronten
 
 	for r := 0; r < nRoundsF/2-1; r++ {
 		for j := 0; j < t; j++ {
-			state[j] = Sigma(api, state[j])
+			state[j] = sigma(api, state[j])
 		}
-		state = Ark(api, state, c, (nRoundsF/2+1)*t+nRoundsP+r*t)
-		state = Mix(api, state, m)
+		state = ark(api, state, c, (nRoundsF/2+1)*t+nRoundsP+r*t)
+		state = mix(api, state, m)
 	}
 
 	for j := 0; j < t; j++ {
-		state[j] = Sigma(api, state[j])
+		state[j] = sigma(api, state[j])
 	}
 
 	for i := 0; i < nOuts; i++ {
-		out[i] = MixLast(api, state, m, i)
+		out[i] = mixLast(api, state, m, i)
 	}
 	return out
 }
