0ba65080a43526199e91073c8f5e2b680dadca63
Lucas Menéndez <hi@lucasmenendez.me>
2023-04-01 14:02:50 +0200

    update compiler to generate new artifacts

--- test/compiler_test.go
+++ test/compiler_test.go
@@ -7,6 +7,8 @@ import (
 
 	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/consensys/gnark-crypto/kzg"
+	"github.com/consensys/gnark/backend/groth16"
+	"github.com/consensys/gnark/backend/plonk"
 	"github.com/consensys/gnark/constraint"
 	cs "github.com/consensys/gnark/constraint/bn254"
 	"github.com/consensys/gnark/frontend"
@@ -15,53 +17,81 @@ import (
 	"github.com/consensys/gnark/test"
 )
 
-func compileGroth16(c frontend.Circuit) (constraint.ConstraintSystem, kzg.SRS, error) {
+func compileGroth16(c frontend.Circuit) (constraint.ConstraintSystem, groth16.ProvingKey, groth16.VerifyingKey, error) {
 	ccs, err := frontend.Compile(ecc.BN254.ScalarField(), r1cs.NewBuilder, c)
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, nil, err
 	}
 
-	srs, err := test.NewKZGSRS(ccs)
+	provingKey, verifyingKey, err := groth16.Setup(ccs)
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, nil, err
 	}
-	return ccs, srs, nil
+
+	return ccs, provingKey, verifyingKey, nil
 }
 
-func saveGroth16(ccs constraint.ConstraintSystem, srs kzg.SRS, ccsDst, srsDst string) error {
-	fdSRS, err := os.Create(srsDst)
-	if err != nil {
-		return err
-	}
-	defer fdSRS.Close()
-	if _, err = srs.WriteTo(fdSRS); err != nil {
-		return err
-	}
-
+func saveGroth16(ccs constraint.ConstraintSystem, provingKey groth16.ProvingKey, verifyingKey groth16.VerifyingKey, ccsDst, pKeyDst, vKeyDst string) error {
 	fdCCS, err := os.Create(ccsDst)
 	if err != nil {
 		return err
 	}
 	defer fdCCS.Close()
 	_r1cs := ccs.(*cs.R1CS)
-	_, err = _r1cs.WriteTo(fdCCS)
-	return err
+	if _, err := _r1cs.WriteTo(fdCCS); err != nil {
+		return err
+	}
+
+	fdPKey, err := os.Create(pKeyDst)
+	if err != nil {
+		return err
+	}
+	defer fdPKey.Close()
+	if _, err := provingKey.WriteTo(fdPKey); err != nil {
+		return err
+	}
+
+	fdVKey, err := os.Create(vKeyDst)
+	if err != nil {
+		return err
+	}
+	defer fdVKey.Close()
+	if _, err := verifyingKey.WriteTo(fdVKey); err != nil {
+		return err
+	}
+	return nil
 }
 
-func compilePlonk(c frontend.Circuit) (constraint.ConstraintSystem, kzg.SRS, error) {
+func compilePlonk(c frontend.Circuit) (constraint.ConstraintSystem, kzg.SRS, plonk.ProvingKey, plonk.VerifyingKey, error) {
 	ccs, err := frontend.Compile(ecc.BN254.ScalarField(), scs.NewBuilder, c)
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, nil, nil, err
 	}
 
 	srs, err := test.NewKZGSRS(ccs)
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, nil, nil, err
 	}
-	return ccs, srs, nil
+
+	provingKey, verifyingKey, err := plonk.Setup(ccs, srs)
+	if err != nil {
+		return nil, nil, nil, nil, err
+	}
+
+	return ccs, srs, provingKey, verifyingKey, nil
 }
 
-func savePlonk(ccs constraint.ConstraintSystem, srs kzg.SRS, ccsDst, srsDst string) error {
+func savePlonk(ccs constraint.ConstraintSystem, srs kzg.SRS, provingKey plonk.ProvingKey, verifyingKey plonk.VerifyingKey, ccsDst, srsDst, pKeyDst, vKeyDst string) error {
+	fdCCS, err := os.Create(ccsDst)
+	if err != nil {
+		return err
+	}
+	defer fdCCS.Close()
+	_scs := ccs.(*cs.SparseR1CS)
+	if _, err := _scs.WriteTo(fdCCS); err != nil {
+		return err
+	}
+
 	fdSRS, err := os.Create(srsDst)
 	if err != nil {
 		return err
@@ -71,41 +101,51 @@ func savePlonk(ccs constraint.ConstraintSystem, srs kzg.SRS, ccsDst, srsDst stri
 		return err
 	}
 
-	fdCCS, err := os.Create(ccsDst)
+	fdPKey, err := os.Create(pKeyDst)
 	if err != nil {
 		return err
 	}
-	defer fdCCS.Close()
+	defer fdPKey.Close()
+	if _, err := provingKey.WriteTo(fdPKey); err != nil {
+		return err
+	}
 
-	_scs := ccs.(*cs.SparseR1CS)
-	_, err = _scs.WriteTo(fdCCS)
-	return err
+	fdVKey, err := os.Create(vKeyDst)
+	if err != nil {
+		return err
+	}
+	defer fdVKey.Close()
+	if _, err := provingKey.WriteTo(fdVKey); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func TestCompileGroth16(t *testing.T) {
-	if ccs, srs, err := compileGroth16(&TestMiMCCircuit{}); err != nil {
+	if ccs, pkey, vkey, err := compileGroth16(&TestMiMCCircuit{}); err != nil {
 		log.Fatal(err)
-	} else if err := saveGroth16(ccs, srs, "./artifacts/mimc_groth16.ccs", "./artifacts/mimc_groth16.srs"); err != nil {
+	} else if err := saveGroth16(ccs, pkey, vkey, "./artifacts/mimc_groth16.ccs", "./artifacts/mimc_groth16.pkey", "./artifacts/mimc_groth16.vkey"); err != nil {
 		log.Fatal(err)
 	}
 
-	if ccs, srs, err := compileGroth16(&TestPoseidonCircuit{}); err != nil {
+	if ccs, pkey, vkey, err := compileGroth16(&TestPoseidonCircuit{}); err != nil {
 		log.Fatal(err)
-	} else if err := saveGroth16(ccs, srs, "./artifacts/poseidon_groth16.ccs", "./artifacts/poseidon_groth16.srs"); err != nil {
+	} else if err := saveGroth16(ccs, pkey, vkey, "./artifacts/poseidon_groth16.ccs", "./artifacts/poseidon_groth16.pkey", "./artifacts/poseidon_groth16.vkey"); err != nil {
 		log.Fatal(err)
 	}
 }
 
 func TestCompilePlonk(t *testing.T) {
-	if ccs, srs, err := compilePlonk(&TestMiMCCircuit{}); err != nil {
+	if ccs, srs, pkey, vkey, err := compilePlonk(&TestMiMCCircuit{}); err != nil {
 		log.Fatal(err)
-	} else if err := savePlonk(ccs, srs, "./artifacts/mimc_plonk.ccs", "./artifacts/mimc_plonk.srs"); err != nil {
+	} else if err := savePlonk(ccs, srs, pkey, vkey, "./artifacts/mimc_plonk.ccs", "./artifacts/mimc_plonk.srs", "./artifacts/mimc_plonk.pkey", "./artifacts/mimc_plonk.vkey"); err != nil {
 		log.Fatal(err)
 	}
 
-	if ccs, srs, err := compilePlonk(&TestPoseidonCircuit{}); err != nil {
+	if ccs, srs, pkey, vkey, err := compilePlonk(&TestPoseidonCircuit{}); err != nil {
 		log.Fatal(err)
-	} else if err := savePlonk(ccs, srs, "./artifacts/poseidon_plonk.ccs", "./artifacts/poseidon_plonk.srs"); err != nil {
+	} else if err := savePlonk(ccs, srs, pkey, vkey, "./artifacts/poseidon_plonk.ccs", "./artifacts/poseidon_plonk.srs", "./artifacts/poseidon_plonk.pkey", "./artifacts/poseidon_plonk.vkey"); err != nil {
 		log.Fatal(err)
 	}
 }
